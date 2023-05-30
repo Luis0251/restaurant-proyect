54d73d26d94ccad6d12f75bd2a350a621bbe9d0d
Luis0251 <luis0251@hotmail.com>
2023-05-30 17:44:36 -0500

    feature:finish menu component

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
-
+import PropTypes from 'prop-types'
 import './MenuItem.css';
 
-export const MenuItem = () => (
-  <div>
-    MenuItem
+export const MenuItem = ({title,price,tags}) => (
+  <div className="app__menuitem">
+  <div className="app__menuitem-head">
+    <div className="app__menuitem-name">
+      <p className="p__cormorant" style={{ color: '#DCCA87' }}>{title}</p>
+    </div>
+    <div className="app__menuitem-dash" />
+    <div className="app__menuitem-price">
+      <p className="p__cormorant">{price}</p>
+    </div>
   </div>
+
+  <div className="app__menuitem-sub">
+    <p className="p__opensans" style={{ color: '#AAAAAA' }}>{tags}</p>
+  </div>
+</div>
 );
 
 
+MenuItem.propTypes = {
+title: PropTypes.string,
+price: PropTypes.string,
+tags:PropTypes.string
+}
