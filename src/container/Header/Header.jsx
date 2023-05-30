e7a6d10a530264378196154fd6e2f9a722c973e3
Luis0251 <luis0251@hotmail.com>
2023-05-30 11:36:36 -0500

    feature:added prop-types and finish header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
-
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Header.css';
 
 export const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper_info'>
+       <SubHeading title="Chase the new flavour"/>
+       <h1 className='app__header-h1'>The Key to Fine Dining</h1> 
+       <p className='p__opensans' style={{margin:'2rem 0'}}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo quibusdam doloremque magnam rerum accusantium sed consequuntur distinctio dolorem iure</p>
+       <button className='custom__button' type='button'>Explore Menu</button> 
+    </div>
+    <div className='app__wrapper_img'>
+      <img src={images.welcome} alt='header img'/>
+    </div>
   </div>
 );
-
 
