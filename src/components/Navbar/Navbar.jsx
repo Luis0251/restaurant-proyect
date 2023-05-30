8b9b558eda26f4c14caa9d616a67f6eac063daae
Luis0251 <luis0251@hotmail.com>
2023-05-30 10:40:16 -0500

    feature:navbar finish

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,74 @@
-import {GiHamburgerMenu} from 'react-icons/gi'
-import {mdOutlineRestaurantMenu} from 'react-icons/md'
-import  images from '../../constants/images';
-import './Navbar.css'
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+import images from "../../constants/images";
+import "./Navbar.css";
 
-import './Navbar.css';
+import "./Navbar.css";
+import { useState } from "react";
 
-export const Navbar = () => (
-  <div className='app__navbar'>
-    Navbar
-  </div>
-);
-
+export const Navbar = () => {
+const [toggleMenu, setToggleMenu] = useState(false);
+const handleClick = () => (setToggleMenu(prevState => !prevState))
+return(
+  <nav className="app__navbar">
+    <div className="app__navbar-logo">
+      <img src={images.gericht} alt="app logo" />
+    </div>
+    <ul className="app__navbar-links">
+      <li className="p__opensans">
+        <a href="#home">Home</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#about">About</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#menu">Menu</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#awards">Awards</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#contact">Contact</a>
+      </li>
+    </ul>
+    <div className="app__navbar-login">
+      <a href="#login" className="p__opensans">
+        Log In / Register
+      </a>
+      <div />
+      <a href="/" className="p__opensans">
+        Book Table
+      </a>
+    </div>
+    <div className="app__navbar-smallscreen">
+      <GiHamburgerMenu color="#fff" fontSize={27} onClick={handleClick} />
+      {toggleMenu && (
+      <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+        <MdOutlineRestaurantMenu
+          fontSize={27}
+          className="overlay__close"
+          onClick={handleClick}
+        />
+        <ul className="app__navbar-smallscreen_links">
+          <li className="p__opensans">
+            <a href="#home">Home</a>
+          </li>
+          <li className="p__opensans">
+            <a href="#about">About</a>
+          </li>
+          <li className="p__opensans">
+            <a href="#menu">Menu</a>
+          </li>
+          <li className="p__opensans">
+            <a href="#awards">Awards</a>
+          </li>
+          <li className="p__opensans">
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+      )}
+    </div>
+  </nav>
+) 
+};
